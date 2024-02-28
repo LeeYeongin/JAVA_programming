34a32089207c65347e76d9905aefc4996d403ad8
LeeYeongin <youngin5757@gmail.com>
2024-02-28 09:20:27 +0900

    구구단 문제2 코드수정

--- edu/src/practice/Gugudan.java
+++ edu/src/practice/Gugudan.java
@@ -12,19 +12,25 @@ public class Gugudan {
 	
 	public void printColumn(int col) {
 		int n = 8/col;
-		int j = 2;
-		int tmp = j + col;
+		int start = 2;
+		int tmp = start + col;
 		
 		if(8 % col != 0)
 			n += 1;
 		
 		for(int k=0; k<n; k++) {
 			for(int i=1; i<10; i++) {
-				for(; j<tmp; j++) {
+				for(int j=start; j<tmp; j++) {
 					System.out.print(j + "*" + i + "=" + (i*j) + "\t");
 				}
 				System.out.println();
 			}
+			start = tmp;
+			tmp += col;
+			
+			if(tmp > 10)
+				tmp = 10;
+			System.out.println();
 		}
 		
 	}
--- edu/src/practice/GugudanTest.java
+++ edu/src/practice/GugudanTest.java
@@ -4,7 +4,7 @@ public class GugudanTest {
 	public static void main(String[] args) {
 		Gugudan ggd = new Gugudan();
 		
-		ggd.printHorizontal();
-		ggd.printColumn(3);
+//		ggd.printHorizontal();
+		ggd.printColumn(4);
 	}
 }
