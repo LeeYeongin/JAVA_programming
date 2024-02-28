f09ed4795f8208d66ad838963c1abc0b0e9715f6
LeeYeongin <youngin5757@gmail.com>
2024-02-28 17:58:34 +0900

    구구단 세로로 출력 코드 추가

--- edu/src/practice/Gugudan.java
+++ edu/src/practice/Gugudan.java
@@ -1,6 +1,22 @@
 package practice;
 
 public class Gugudan {
+	
+	// 단 하나를 출력하는 메소드
+	public void print(int num) {
+		for(int i=1; i<10; i++)
+			System.out.println(num + " * " + i + " = " + (num*i));
+	}
+	// 구구단을 순서대로 출력하는 메소드
+	public void printVertical() {
+	// print 메소드를 사용하도록 코드 작성
+		for(int i=2; i<10; i++) {
+			print(i);
+			System.out.println();
+		}
+	}
+	
+	// 구구단을 가로로 순서대로 출력하는 메소드
 	public void printHorizontal() {
 		for(int i=1; i<10; i++) {
 			for(int j=2; j<10; j++) {
@@ -10,6 +26,7 @@ public class Gugudan {
 		}
 	}
 	
+	// 원하는 col수 만큼 가로로 출력하는 메소드
 	public void printColumn(int col) {
 		int n = 8/col;
 		int start = 2;
--- edu/src/practice/GugudanTest.java
+++ edu/src/practice/GugudanTest.java
@@ -4,6 +4,7 @@ public class GugudanTest {
 	public static void main(String[] args) {
 		Gugudan ggd = new Gugudan();
 		
+//		ggd.printVertical();
 //		ggd.printHorizontal();
 		ggd.printColumn(4);
 	}
