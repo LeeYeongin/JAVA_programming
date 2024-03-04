b03747fcf00f42a55e14a22b196f63a27d7b334c
LeeYeongin <youngin5757@gmail.com>
2024-03-04 17:59:44 +0900

    최소 지폐 수 계산 코드 작성중

---
+++ edu/src/practice/MiniBillCount.java
@@ -0,0 +1,18 @@
+package practice;
+
+import java.util.Scanner;
+
+public class MiniBillCount {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("금액을 입력하세요: ");
+		int price = sc.nextInt();
+		
+		int fiveM = 0;
+		int oneM = 0;
+		int fiveT = 0;
+		int oneT = 0;
+		
+		
+	}
+}
